c1f2c5eed43272f03487de302ecd4d6c846c3a26
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-17 23:10:57 +0800

    setup material ui theme (light/dark) mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { useSelector } from 'react-redux';
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return <div className='app'>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      app
+    </ThemeProvider>
+  </div>
+  
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { configureStore } from '@reduxjs/toolkit';
-import globalReducer from "state";
+import globalReducer from "./state";
 import { Provider } from 'react-redux';
 
 const store = configureStore({
