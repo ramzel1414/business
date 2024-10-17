e258ed496087c28477e8bb89d91c4dfeb1e3a8ca
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-17 22:51:06 +0800

    redux toolkit boiler plate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from "state";
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
