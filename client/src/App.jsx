49d925c8d17775dbdb02b0f23a4fa6e6298973f4
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-17 23:27:08 +0800

    setup navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,29 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
 import { useSelector } from 'react-redux';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Layout from "./scenes/layout";
+import Dashboard from "./scenes/dashboard";
 
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return <div className='app'>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      app
-    </ThemeProvider>
-  </div>
-  
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" elemnt={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  ) 
 }
 
 export default App
