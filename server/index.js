2786df7f629f1726057e8d6c5bfe4dd56213fa02
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-18 18:06:51 +0800

    inject Product and ProductStat into mongoDB

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from './models/User.js';
-import { dataUser } from './data/index.js';
+import Product from './models/Product.js';
+import ProductStat from './models/ProductStat.js';
+import { dataUser, dataProduct, dataProductStat } from './data/index.js';
 
 // Configurations
 dotenv.config();
@@ -39,6 +41,8 @@ mongoose.connect(process.env.MONGO_URL, {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
   // only add data one time
+  Product.insertMany(dataProduct);
+  ProductStat.insertMany(dataProductStat);
   // User.insertMany(dataUser);
 
 }).catch((error) => console.log(`${error} did not connect`));
