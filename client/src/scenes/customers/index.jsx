3ec890cc8b6bb9924e16bfa6420d46e6fe02c57f
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-18 19:34:33 +0800

    setup customers route and test w/ GET method

---
+++ client/src/scenes/customers/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from "../../components/Header";
+
+const Customers = () => {
+  return (
+    <Header title="CUSTOMERS" subtitle="we are the customers"/>
+  )
+}
+
+export default Customers
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products"],
+  tagTypes: ["User", "Products", "Customers"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -13,10 +13,14 @@ export const api = createApi({
       query: () => "client/products",
       providesTags: ["Products"],
     }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
   })
 })
 
 
 export const {
-  useGetUserQuery, useGetProductsQuery,
+  useGetUserQuery, useGetProductsQuery, useGetCustomersQuery
 } = api;
