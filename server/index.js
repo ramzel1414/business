8de1657b2871c2fa76ae6fc79ad6ed5f192e1494
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-20 12:44:24 +0800

    inject dataAffiliateStat into database

--- server/index.js
+++ server/index.js
@@ -17,12 +17,15 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
 import { 
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction, 
   dataOverallStat,
+  dataAffiliateStat,
+  
 } from './data/index.js';
 
 // Configurations
@@ -49,10 +52,12 @@ mongoose.connect(process.env.MONGO_URL, {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
   // ONLY ADD DATA ONE TIME
+  // AffiliateStat.insertMany(dataAffiliateStat);
   // OverallStat.insertMany(dataOverallStat);
   // Product.insertMany(dataProduct);
   // ProductStat.insertMany(dataProductStat);
   // Transaction.insertMany(dataTransaction);
   // User.insertMany(dataUser);
+
 
 }).catch((error) => console.log(`${error} did not connect`));
